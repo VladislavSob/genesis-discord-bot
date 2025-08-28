1fc0868d37d6b230786b7d36399bd2fdb6ccccae
Genuyss <sobetskiyvladislav@gmail.com>
2025-08-29 05:45:19 +0800

    Обновленные системы ролей и названия парсинга

--- Батник/push_bot.bat
+++ Батник/push_bot.bat
@@ -1,46 +1,42 @@
 @echo off
+chcp 65001 >nul
 cd C:\Users\Genesis\Downloads\Genesis_BOT
 
 echo ==============================
-echo   Проверка Git-репозитория...
+echo  Проверка Git-репозитория...
 echo ==============================
 
-REM Проверяем, инициализирован ли git
+REM init при необходимости
 if not exist .git (
-    echo Git-репозиторий не найден. Инициализирую...
-    git init
+  git init
 )
 
-REM Проверяем, есть ли remote "origin"
+REM remote origin при необходимости
 git remote get-url origin >nul 2>&1
 if errorlevel 1 (
-    echo Remote origin не найден. Добавляю...
-    git remote add origin https://github.com/VladislavSob/genesis-discord-bot.git
+  git remote add origin https://github.com/VladislavSob/genesis-discord-bot.git
 )
 
-REM Проверяем, есть ли ветка main
-for /f "tokens=*" %%i in ('git branch --show-current') do set BRANCH=%%i
-if "%BRANCH%"=="" (
-    echo Ветка не выбрана. Устанавливаю main...
-    git branch -M main
-)
+REM гарантируем ветку main
+for /f "tokens=*" %%i in ('git branch --show-current') do set BR=%%i
+if "%BR%"=="" git branch -M main
 
-echo ==============================
-echo   Добавляю изменения...
-echo ==============================
+echo.
+set /p MSG=Сообщение коммита (можно по-русски): 
+if "%MSG%"=="" set MSG=Обновление кода бота
+
+echo.
+echo Добавляю изменения...
 git add .
 
-echo ==============================
-echo   Делаю коммит...
-echo ==============================
-git commit -m "Авто-обновление кода бота" || echo Нет изменений для коммита.
+echo Делаю коммит...
+git commit -m "%MSG%" || echo Нет изменений для коммита.
 
-echo ==============================
-echo   Отправляю на GitHub...
-echo ==============================
+echo Отправляю на GitHub...
 git push -u origin main
 
+echo.
 echo ==============================
-echo   Готово! Код отправлен :)
+echo  Готово! Код отправлен :)
 echo ==============================
 pause
